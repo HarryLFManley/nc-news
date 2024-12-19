997a4159cf4f7c0a2a303d28a9fc96a477f6a4bf
Harry Manley <HarryLFManley@outlook.com>
2024-12-19 16:33:32 +0000

    Added Votes to articles

--- src/ArticlePage.jsx
+++ src/ArticlePage.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { addVotes } from "../utils/api";
+import { GetArticle } from "../utils/api";
+import { useParams } from "react-router-dom";
+
 const ArticlePage = ({ article, comments }) => {
+  const { article_id } = useParams();
+  const [votes, setVotes] = useState(article.votes);
+
+  useEffect(() => {
+    GetArticle(article_id).then((response) => {
+      setVotes(response.article.article.votes);
+    });
+  }, []);
+
+  const HandleVotes = () => {
+    addVotes(article_id, { inc_votes: 1 })
+      .then((response) => {
+        setVotes(response.article.votes);
+      })
+      .catch(() => {
+        setVotes(votes);
+      });
+  };
 
   return (
     <>
@@ -8,6 +31,9 @@ const ArticlePage = ({ article, comments }) => {
         <p>{article.body}</p>
         <p className="bold">by {article.author}</p>
       </section>
+
+      <p>Votes: {votes}</p>
+      <button onClick={HandleVotes}>Vote</button>
 
       <h3>Comments</h3>
       <hr />
